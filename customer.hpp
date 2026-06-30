4ec3ac09406e38ddd5838cbabc1cb0c479630ea6
Timothyhsandbox <timothydanhines@gmail.com>
2026-06-30 09:32:11 +0700

    Added custermer class file and header

---
+++ customer.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+class Customer{
+private:
+    std::string customer_ID;
+    std::string first_name;
+    std::string last_name;
+    std::string phone;
+    int pin;
+    std::string email;
+public:
+    
+
+};
