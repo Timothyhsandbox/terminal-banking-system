9771e5101d73ccca345fcd02b2ef2957e7421f1a
Timothyhsandbox <timothydanhines@gmail.com>
2026-07-01 14:43:55 +0700

    Made changes to customer.cpp file.

--- customer.cpp
+++ customer.cpp
@@ -1,26 +1,78 @@
 #include "customer.hpp"
 
 #include <string>
+#include <iostream>
+
+std::string inputfirstname();
+std::string inputmiddlename();
+std::string inputlastname();
+std::string inputemail();
+std::string inputid();
 
 std::ostream& operator<<(std::ostream &os,const Customer &cus) {
     os << cus.first_name << " " << cus.middle_name << " " << cus.last_name << std::endl;
     return os;
 }
 
-Customer::Customer(std::string customer_id,std::string first_name,std::string middle_name,std::string last_name,std::string pin,std::string email)
-    : customer_id{customer_id},first_name{first_name},middle_name{middle_name},last_name{last_name},pin{pin},email{email} {};
+Customer::Customer()
+    : first_name(inputfirstname()),
+     middle_name(inputmiddlename()),
+     last_name(inputlastname()),
+     customer_id(inputid()),
+     email(inputemail()) {
+        std::cout << "constructor called." << std::endl;
+    };
 
-std::string Customer::get_name() const {
-    if(middle_name == "-")
-        return first_name + " " + last_name;
-    else
-        return first_name + " " + middle_name + " " + last_name;
-};
-
-std::string Customer::get_pin() const {
-    return pin;
+std::string Customer::get_email() const {
+    return email;
 }
 
 std::string Customer::get_id() const {
     return customer_id;
 }
+
+std::string inputfirstname(){
+    std::string firstname;
+
+    std::cout << "Enter firstname: ";
+    std::getline(std::cin,firstname);       
+    return firstname;
+}
+
+std::string inputmiddlename(){
+    std::string middlename;
+
+    std::cout << "Enter middlename(If you do not have middlename input -): ";
+    std::getline(std::cin,middlename);
+    
+    if(middlename == "-")
+        return "";
+    else
+        return middlename;
+}
+
+std::string inputlastname(){
+    std::string lastname;
+
+    std::cout << "Enter Lastname: ";
+    std::getline(std::cin,lastname);
+
+    return lastname;
+}
+
+std::string inputemail(){
+    std::string email;
+
+    std::cout << "Enter email: ";
+    std::getline(std::cin,email);
+    return email;
+}
+
+std::string inputid(){
+    std::string id;
+
+    std::cout << "Enter your id: ";
+    std::getline(std::cin,id);
+    return id;
+}
+
--- customer.hpp
+++ customer.hpp
@@ -3,17 +3,16 @@
 #include <string>
 
 class Customer{
-friend std::ostream& operator<<(std::ostream &os,const Customer &c);
+friend std::ostream& operator<<(std::ostream &os,const Customer &cus);
 private:
-    std::string customer_id;
     std::string first_name;
     std::string middle_name; //If do not have middle name input must be "-" string.
     std::string last_name;
-    std::string pin;
+    std::string customer_id;
     std::string email;
 public:
-    Customer(std::string customer_id,std::string first_name,std::string middle_name,std::string last_name,std::string pin,std::string email);
-    std::string get_name() const;
-    std::string get_pin() const;
+    Customer();
+    std::string get_email() const;
     std::string get_id() const;
+
 };
